202cab0fd3ee3191ebe7cace797c955d55fcf270
Andrew Tapper <andrew@tapper.codes>
2022-12-05 09:32:25 -0600

    :white_check_mark: update tests

--- src/app/@shared/components/podcast-listing/podcast-listing.component.spec.ts
+++ src/app/@shared/components/podcast-listing/podcast-listing.component.spec.ts
@@ -1,50 +1,35 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { PodcastListingComponent } from "./podcast-listing.component";
-import * as MockPodcast from '@core/models/mocks/podcast.mock.json';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { PodcastListingComponent } from './podcast-listing.component'
+import * as MockPodcast from '@core/models/mocks/podcast.mock.json'
 
 describe('@shared/PodcastListingComponent', () => {
-  let component: PodcastListingComponent;
-  let fixture: ComponentFixture<PodcastListingComponent>;
+  let component: PodcastListingComponent
+  let fixture: ComponentFixture<PodcastListingComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PodcastListingComponent ]
-    })
-    .compileComponents();
-  }));
+      declarations: [PodcastListingComponent],
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PodcastListingComponent);
+    fixture = TestBed.createComponent(PodcastListingComponent)
 
-    component = fixture.componentInstance;
-    component.index = 0;
-    component.podcast = MockPodcast;
+    component = fixture.componentInstance
+    component.index = 0
+    component.podcast = MockPodcast
 
-    fixture.detectChanges();
-  });
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    expect(component).toBeTruthy()
+  })
 
-  it('should have generic p tag', () => {
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.podcast__container p').textContent).toContain('Podcast details will go in here');
-  });
-
-  it('on podcast data change, should still have generic p tag', () => {
-    component.podcast.title = "New Mock Title";
-
-    fixture.detectChanges();
-
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.podcast__container p').textContent).toContain('Podcast details will go in here');
-  });
-
-  describe('viewPodcast()',() => {
+  describe('viewPodcast()', () => {
     it('should return false', () => {
-      var result = component.viewPodcast();
-      expect(result).toBeFalse();
-    });
-  });
+      var result = component.viewPodcast()
+      expect(result).toBeFalse()
+    })
+  })
 })
